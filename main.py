3e48a7c70b65f757659ac9044047ddb6d50ef372
sebawyszkow08 <seba.wyszkow08@gmail.com>
2024-11-15 18:44:59 +0100

    poruszanie

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ screen = pygame.display.set_mode([screen_w,screen_h])
 
 clock = pygame.time.Clock()
 
-
+plx = 270
+ply = 130
 #petla gry
 game_on = True
 while game_on == True:
@@ -31,15 +32,32 @@ while game_on == True:
             if event.key == pygame.K_ESCAPE:
                 game_on = False
 
+
+
+            elif event.key == pygame.K_DOWN:
+                ply += 10
+
+            elif event.key == pygame.K_UP:
+                ply -= 10
+            elif event.key == pygame.K_LEFT:
+                plx -= 10
+
+            elif event.key == pygame.K_RIGHT:
+                plx += 10
+
+
+
+
         if event.type==pygame.QUIT:
             game_on = False
 
+        
 
 
 
     screen.blit(background_image,(0,0))
 
-    screen.blit(character_image,(270,130))
+    screen.blit(character_image,(plx,ply))
 
     pygame.display.flip()
 
